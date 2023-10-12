2b4b7dd5b27c04de7c3385c73a122150e36916c5
mushrune <112779587+mushrune@users.noreply.github.com>
2023-10-12 12:59:42 -0700

    Added tailwind and formatted the tool window

--- oce-react-app/src/Map.tsx
+++ oce-react-app/src/Map.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import './Map.css';
+import './/Map.css'
+import {TbWorldLatitude, TbWorldLongitude, TbZoomPan} from "react-icons/tb";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN as string;
 
@@ -16,11 +17,25 @@ const Map: React.FC = () => {
         const map = new mapboxgl.Map({
             container: mapContainerRef.current,
             style: 'mapbox://styles/mapbox/streets-v11',
+            projection: { name: 'globe' },
             center: [ longitude, latitude ],
             zoom: zoom
         })
 
         map.addControl( new mapboxgl.NavigationControl(), 'top-right' );
+
+        map.on('style.load', () => {
+            map.addSource('mapbox-dem', {
+                'type': 'raster-dem',
+                'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+                'tileSize': 512,
+                'maxzoom': 14
+            });
+
+            map.setTerrain({
+                'source': 'mapbox-dem'
+            });
+        })
 
         map.on('move', () => {
             setLatitude( parseFloat( map.getCenter().lat.toFixed( 4 ) ) );
@@ -33,12 +48,36 @@ const Map: React.FC = () => {
 
     return (
         <div>
-            <div className='sidebarStyle'>
-                <div>
-                    Longitude: {longitude} | Latitude: {latitude} | Zoom: {zoom}
+            <div className="sidebar-container">
+                <div className="
+                    bg-indigo-800
+                    rounded-lg
+                    border-2 border-indigo-900
+                    p-2 m-4
+                    text-white
+                    w-[40%] max-w-xl
+                ">
+                    <h1 className="text-2xl italic mx-2 mb-2">ourchanging.earth</h1>
+                    {/* This portion of the tool shows current lat, lon, and zoom level.*/}
+                    <div className="grid grid-cols-3 p-2 rounded-md bg-indigo-700">
+                        <div className="flex items-center w-full">
+                            <TbWorldLatitude size={30} />
+                            <span className="text-lg font-bold mx-2 italic">{latitude}</span>
+                        </div>
+                        <div className="flex items-center w-full">
+                            <TbWorldLongitude size={30} />
+                            <span className="text-lg font-bold mx-2 italic">{longitude}</span>
+                        </div>
+                        <div className="flex items-center w-full">
+                            <TbZoomPan size={30} />
+                            <span className="text-lg font-bold mx-2 italic">{zoom}</span>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='map-container' ref={mapContainerRef} />
+            <div className="
+                map-container
+            " ref={mapContainerRef} />
         </div>
     )
 }
